62ca426c2029daba6105e235e6b151dc74a92ade
dhameliyamiral8007 <dhameliyamiral8007@gmail.com>
2026-02-12 11:07:06 +0530

    design changes

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,18 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'mui': ['@mui/material', '@mui/x-date-pickers', '@emotion/react', '@emotion/styled'],
+          'maps-leaflet': ['leaflet', 'react-leaflet', '@react-google-maps/api'],
+          'stripe': ['@stripe/stripe-js', '@stripe/react-stripe-js'],
+          'utils': ['lucide-react', 'date-fns', 'dayjs', 'socket.io-client', 'yup'],
+        }
+      }
+    }
+  }
 })
+
