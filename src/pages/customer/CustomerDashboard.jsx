564c2710ac2fd55d1c49306e832105ba0e240ade
rajvi-th <rajvi@techifyhouse.com>
2026-03-25 09:46:18 +0530

    space

--- src/pages/customer/CustomerDashboard.jsx
+++ src/pages/customer/CustomerDashboard.jsx
@@ -264,6 +264,7 @@ const CustomerDashboard = () => {
                 className="w-full h-full object-cover"
               />
             </div>
+            
 
             {/* Content Overlay */}
             <div className="relative z-10 flex flex-col sm:flex-row items-center justify-between gap-3 sm:gap-4">
